15d7d22f5ea3d194026246699b49dff6e64d71f6
Dmitry Kulakov <atomeistee@gmail.com>
2022-12-13 14:01:42 +0300

    graphql and vercel

--- api/graphql.js
+++ api/graphql.js
@@ -6,13 +6,6 @@ import resolvers from "../server/resolvers";
 const apolloServer = new ApolloServer({
   typeDefs,
   resolvers,
-  context: async () => {
-    const dbConn = await mongoose.createConnection(
-      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.esoz4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-    );
-
-    return { dbConn };
-  },
   playground: true,
   introspection: true,
 });
