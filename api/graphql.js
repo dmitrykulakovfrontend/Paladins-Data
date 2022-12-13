396342a4aee1c9ae60f907f9a763e5ecb676219b
Dmitry Kulakov <atomeistee@gmail.com>
2022-12-13 13:38:12 +0300

    connect graphql to vercel

--- api/graphql.js
+++ api/graphql.js
@@ -1,3 +1,21 @@
-export default async function graphql(req, res) {
-  res.redirect("https://studio.apollographql.com/sandbox/explorer");
-}
+import { ApolloServer } from "@apollo/server";
+import { getConnection } from "../src/database";
+
+import typeDefs from "../server/typeDefs";
+import resolvers from "../server/resolvers";
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async () => {
+    const dbConn = await mongoose.createConnection(
+      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.esoz4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    );
+
+    return { dbConn };
+  },
+  playground: true,
+  introspection: true,
+});
+
+export default apolloServer.createHandler({ path: "/api/graphql" });
